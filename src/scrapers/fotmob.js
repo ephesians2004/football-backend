73f2e0d10e92e2370b5941f4aae84e19a8797d77
ephesians2004 <ephesians2004@gmail.com>
2025-12-30 20:29:46 +0000

    Update fotmob.js

--- src/scrapers/fotmob.js
+++ src/scrapers/fotmob.js
@@ -1,10 +1,13 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = async function(){
   try{
-    const r = await axios.get("https://www.fotmob.com/api/matches");
+    const r = await axios.get(
+      "https://www.fotmob.com/api/matches?timezone=UTC",
+      { headers: { "User-Agent": "Mozilla/5.0" } }
+    );
     return r?.data?.matches || [];
-  }catch(e){
+  } catch (e) {
     return [];
   }
 }
