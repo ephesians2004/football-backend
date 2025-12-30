2bbd34d1ace3a08f08bb4999637328d203049b5a
ephesians2004 <ephesians2004@gmail.com>
2025-12-30 20:38:09 +0000

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { latestData } = require('../cron/scrapeAll');
 
-router.get('/data', (_req,res)=>res.json(latestData()));
+router.get('/data', (_req,res)=> res.json(latestData()));
 
 module.exports = router;
