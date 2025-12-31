4cffaa874d8a4e368fb113df7cbe2356f4ef7218
ephesians2004 <ephesians2004@gmail.com>
2025-12-31 13:48:33 +0000

    Update footballdata.js

--- src/scrapers/footballdata.js
+++ src/scrapers/footballdata.js
@@ -1,32 +1,35 @@
 const { get } = require("../utils/http");
-
 const KEY = process.env.FOOTBALLDATA_KEY;
 
-async function fetchToday() {
-  const today = new Date().toISOString().split("T")[0];
+async function fetchRange(days = 7) {
+  const now = new Date();
+  const today = now.toISOString().split("T")[0];
 
-  const leagues = ["PL","PD","SA","BL1","FL1","DED","BSA","PPL"]; // global
+  const future = new Date();
+  future.setDate(now.getDate() + days);
+  const to = future.toISOString().split("T")[0];
+
+  const leagues = ["PL","PD","SA","BL1","FL1","DED","BSA","PPL"]; // add more anytime
   let all = [];
 
   for (let code of leagues) {
-    const url = `https://api.football-data.org/v4/competitions/${code}/matches?dateFrom=${today}&dateTo=${today}`;
-    const data = await get(url, { "X-Auth-Token": KEY });
+    const url = `https://api.football-data.org/v4/competitions/${code}/matches?dateFrom=${today}&dateTo=${to}`;
 
+    const data = await get(url, { "X-Auth-Token": KEY });
     if (!data?.matches) continue;
 
-    const mapped = data.matches.map(m => ({
-      id: m.id,
-      home: m.homeTeam?.name,
-      away: m.awayTeam?.name,
-      date: m.utcDate,
-      league: m.competition?.name,
-      status: m.status
-    }));
-
-    all.push(...mapped);
+    all.push(
+      ...data.matches.map(m => ({
+        id: m.id,
+        home: m.homeTeam?.name,
+        away: m.awayTeam?.name,
+        date: m.utcDate,
+        league: m.competition?.name,
+        status: m.status
+      }))
+    );
   }
-
   return all;
 }
 
-module.exports = fetchToday;
+module.exports = fetchRange;
